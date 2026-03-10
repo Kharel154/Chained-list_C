246b35e3a3ed49a69eb4a16eb18afb32d2b61d9d
Kharel154 <kharelbiloa@gmail.com>
2026-03-10 19:56:19 +0100

    2nd commit: Menu added to my chained list

--- chainedlist.c
+++ chainedlist.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 
 typedef struct List{
 	int  num;
@@ -35,13 +35,19 @@ List* insertend(List* l, int v){
 			temp=temp->next;
 		}
 		temp->next=node;
-	return l;
+	
 	}
+	return l;
 
 }
 
 
 void display(List* l){
+	if(l==NULL){
+		printf("The list is empty\n");
+		return;
+	}
+	printf("\n");
 	List* temp=l;
 	while(temp){
 		printf("%d -> ",temp->num);
@@ -51,6 +57,10 @@ void display(List* l){
 }
 
 void freelist(List* l){
+	if(l==NULL){
+		printf("The list is already free\n");
+		return;
+	}
 	while(l!=NULL){
 		List* temp=l;
 		l=l->next;
@@ -68,7 +78,61 @@ int main(){
 	list=inserthead(list,4);
 	list=inserthead(list,5);
 	list=insertend(list,10);
+	bool flag=true;
 	
+
 	display(list);
-	freelist(list);
+	
+	while(flag){
+		int choice;
+
+		printf("            ---MENU---               \n");
+		printf("1) Add a number at the head of the list\n");
+		printf("2) Add a number at the end of the list\n");
+		printf("3) Display list\n");
+		printf("4) Free list\n");
+		printf("5) Quit\n");
+		printf("Enter your choice : ");
+		scanf("%d",&choice);
+		switch(choice){
+			int a;
+			case 1:
+				
+				printf("\nEnter the number : ");
+				scanf("%d",&a);
+				list=inserthead(list,a);
+				printf("\n %d has been succesfuly inserted at the head \n",a);
+				break;
+
+			case 2:
+				
+                                printf("\nEnter the number : ");
+                                scanf("%d",&a);
+                                list=insertend(list,a);
+                                printf("\n %d has been succesfuly inserted at the end \n",a);
+				break;
+
+			case 3:
+				display(list);
+				break;
+			case 4:
+                                freelist(list);
+				list=NULL;
+                                printf("The list has been freed succesfully\n");
+                                break;
+			case 5:
+				freelist(list);
+				flag=false;
+				printf("Good bye !\n\n");
+				break;
+			default : 
+				printf("Wrong entry try again ");
+				break;
+
+
+
+		}
+	}
+
+
 }
